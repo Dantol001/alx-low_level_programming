bd4e1e4df51da7d53edb346a1acc78243d9f34f8
Dantol001 <fadadan007@yahoo.com>
2022-10-13 11:49:13 -0700

    4_Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Serves as the entry
+ * Return: Always 0 if no errors
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
