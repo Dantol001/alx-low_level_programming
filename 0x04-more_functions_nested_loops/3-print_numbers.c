b04f7be745ca16de85f7f41b7b84dd18a09d1bd0
Dantol001 <fadadan007@yahoo.com>
2022-10-20 15:52:45 -0700

    The numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
