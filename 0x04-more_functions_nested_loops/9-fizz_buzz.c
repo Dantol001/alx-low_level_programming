3ed7d3457d8efd22af8fc1d5a09922c483560dfc
Dantol001 <fadadan007@yahoo.com>
2022-10-20 17:24:06 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,21 @@ int main(void)
 	for (n = 2; n <= 100; n++)
 	{
 		if (n % 3 == 0)
+		{
 			printf("Fizz");
-		if (n % 5 == 0)
+		}
+		else if (n % 5 == 0)
+		{
 			printf("Buzz");
-		if ((n % 3 == 0) && (n % 5 == 0))
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
 		else
+		{
 			printf("%d", n);
+		}
 	}
 	printf("\n");
 	return (0);
